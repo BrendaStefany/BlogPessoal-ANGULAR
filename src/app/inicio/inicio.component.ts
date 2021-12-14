a5b29ca7dddd8974071079993938458e29b08ed4
BrendaStefany <brenda.stefany.cavalcanti@gmail.com>
2021-12-14 20:47:46 -0300

    [@BrendaStefany - Creating method post, get (Postagem)]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,23 +1,86 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  listaPostagens: Postagem[];
+
+  postagem: Postagem = new Postagem();
+  listaTemas: Tema[];
+
+  idTema: number;
+  tema: Tema = new Tema();
+
+  usuario: Usuario = new Usuario();
+  idUsuario = environment.id;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(){
-    // if(environment.token == '') {
-    //   alert('Sua seção expirou, faça o login novamente!')
-    //   this.router.navigate(['/entrar'])
-    // }
+  ngOnInit() {
+    if(environment.token == '') {
+      alert('Sua seção expirou, faça o login novamente!')
+      this.router.navigate(['/entrar'])
+    }
+    this.authService.refreshToken();
+    this.getAllTemas();
+    this.getAllPostagens();
+   
   }
 
+  getAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp;
+    });
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp;
+    });
+  }
+
+  getAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp;
+    });
+  }
+
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUsuario).subscribe((resp: Usuario) => {
+      this.usuario = resp
+    })
+  }
+
+  publicar() {
+    this.tema.id = this.idTema;
+    this.postagem.tema = this.tema;
+
+    this.usuario.id = this.idUsuario;
+    this.postagem.usuario = this.usuario;
+
+    this.postagemService
+      .postPostagem(this.postagem)
+      .subscribe((resp: Postagem) => {
+        this.postagem = resp;
+        alert('Postagem publicada com sucesso!!');
+        this.postagem = new Postagem()
+        this.getAllPostagens()
+      });
+  }
 }
