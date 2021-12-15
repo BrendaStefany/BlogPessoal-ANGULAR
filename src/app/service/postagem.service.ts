cdfd2ea1b3700c9ab87d407e1a6c2d1c559be1fb
BrendaStefany <brenda.stefany.cavalcanti@gmail.com>
2021-12-14 22:34:15 -0300

    [@BrendaStefany - put and delete implementation (Postagem)]

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -21,7 +21,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogpessoalbs.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoalbs.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem> {
     return this.http.post<Postagem>('https://blogpessoalbs.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoalbs.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://blogpessoalbs.herokuapp.com/postagens/${id}`, this.token)
   }
 }
